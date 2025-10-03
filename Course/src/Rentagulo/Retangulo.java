77313dbf9e9160625ed5c87604c6134fed631b8c
Thiago Schutz <thiagoschutz08@gmail.com>
2025-10-03 16:58:50 -0300

    Este é um programa que tem uma classe Retangulo dentro de um pacote retangulo:esta classe lê a largura e a altura do retangulo e calcula a área, perimetro e a diagonal. A classe main instancia a classe Retangulo em uma variavel retangulo, sendo assim lê a largura, altura e print na tela.

---
+++ Course/src/Rentagulo/Retangulo.java
@@ -0,0 +1,19 @@
+package Rentagulo;
+
+public class Retangulo {
+    public double largura;
+    public double altura;
+
+    public double area() {
+        return largura * altura;
+    }
+
+    public double perimetro() {
+        return (largura * 2) + (altura * 2);
+    }
+
+    public double diagonal() {
+        return Math.sqrt((altura * altura) + (largura * largura));
+    }
+
+}
